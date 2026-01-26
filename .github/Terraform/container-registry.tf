898f0eed7ca7a0c2659b492c2d2d4c8aa8a42e17
Fossen <simen.fossen@capgemini.com>
2026-01-26 10:43:53 +0100

    remove dashes from acr

--- .github/Terraform/container-registry.tf
+++ .github/Terraform/container-registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "fozzen-reactdotnet-reg${var.environment}"
+  name                = "fozzenreactdotnetreg${var.environment}" ## no dashes allowed
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Premium"
