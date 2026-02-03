9e0d01dfe48bcc407b35629416bc41539cc76538
Simen Fossen <simen.fosen@lyse.net>
2026-02-03 06:00:09 +0100

    add containeization on terraform creation of app service

--- .github/Terraform/appservice.tf
+++ .github/Terraform/appservice.tf
@@ -2,6 +2,8 @@ resource "azurerm_app_service_plan" "appPlan" {
   name                = "fozzen-reactdotnet-appplan${var.environment}"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
+  kind     = "Linux"
+  reserved = true
 
   sku {
     tier = "Standard"
@@ -17,6 +19,17 @@ resource "azurerm_app_service" "AppSvc" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.appPlan.id
+
+# Set container to a default template
+  site_config {
+    linux_fx_version = "DOCKER|mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+  }
+# Make sure template only set at first creation
+  lifecycle {
+  ignore_changes = [
+    site_config[0].linux_fx_version
+  ]
+}
 
     tags = {
     Environment = var.environment
